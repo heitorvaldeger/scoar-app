8e6fb79db6a3ebf6c8a8e5eb78ba04db43aa88ca
heitorvaldeger <aurelio.heitor@hotmail.com>
2021-03-04 22:55:21 -0300

    Configuração das variaveis de ambiente

--- nuxt.config.js
+++ nuxt.config.js
@@ -77,22 +77,22 @@ export default {
 
   firebase: {
     config: {
-      apiKey: 'AIzaSyDi7FTJplxdcfOjRdkq4in0MsA-MSRLULc',
-      authDomain: 'scoar-app.firebaseapp.com',
+      apiKey: process.env.FIREBASE_API_KEY,
+      authDomain: process.env.FIREBASE_AUTH_DOMAIN,
       databaseURL: (process.env.NODE_ENV === 'production')
-        ? 'https://scoar-app.firebaseio.com'
+        ? process.env.FIREBASE_DATABASE_URL
         : 'http://localhost:9000?ns=scoar-app',
-      projectId: 'scoar-app',
-      storageBucket: 'scoar-app.appspot.com',
-      messagingSenderId: '863908883910',
-      appId: '1:863908883910:web:809f00cd5b6a123c6c16c7',
-      measurementId: 'G-RG65LNDC1C'
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+      messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+      appId: process.env.FIREBASE_APP_ID,
+      measurementId: process.env.FIREBASE_MEASUREMENT_ID
     },
     services: {
       database: true,
       functions: (process.env.NODE_ENV !== 'production') ? {
-        location: 'us-central1',
-        emulatorPort: 5001,
+        location: process.env.FIREBASE_FUNCTIONS_LOCALE,
+        emulatorPort: process.env.FIREBASE_FUNCTIONS_PORT,
         emulatorHost: 'http://localhost'
       } : true,
       auth: {
