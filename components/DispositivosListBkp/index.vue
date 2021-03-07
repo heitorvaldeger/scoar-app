090079d750273b26293ea92a06565abde1172f27
heitorvaldeger <aurelio.heitor@hotmail.com>
2021-03-06 23:21:40 -0300

    Atualização do layout da listagem de dispositivos

---
+++ components/DispositivosListBkp/index.vue
@@ -0,0 +1,80 @@
+<template>
+  <span>
+    <v-row>
+      <v-col v-for="(item, index) in dispositivos" :key="index" cols="auto">
+        <dispositivos-item :item="item">
+          <template v-if="isAdmin" v-slot:menu>
+            <v-menu offset-y>
+              <template v-slot:activator="{ on, attrs }">
+                <v-btn
+                  icon
+                  v-bind="attrs"
+                  v-on="on"
+                >
+                  <v-icon>mdi-menu</v-icon>
+                </v-btn>
+              </template>
+              <v-list>
+                <v-list-item @click="dispositivosEdit(item)">
+                  <v-list-item-title>
+                    Editar
+                  </v-list-item-title>
+                </v-list-item>
+                <v-list-item @click="remove(item['.key'])">
+                  <v-list-item-title>Apagar</v-list-item-title>
+                </v-list-item>
+              </v-list>
+            </v-menu>
+          </template>
+        </dispositivos-item>
+      </v-col>
+    </v-row>
+  </span>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+
+export default {
+  name: 'DispositivosList',
+  data () {
+    return ({
+      isAdmin: false
+    })
+  },
+  computed: {
+    ...mapState({
+      dispositivos: state => state.dispositivos.all
+    })
+  },
+  created () {
+    this.isAdmin = this.$store.getters['auth/isAdmin']
+    this.$store.dispatch('dispositivos/getDispositivos')
+  },
+  methods: {
+    dispositivosEdit (item) {
+      this.$store.commit('dialog/DIALOG_OPEN', {
+        component: 'DispositivosEdit',
+        data: item
+      })
+    },
+    remove (dispositivoKey) {
+      this.$store.dispatch('dispositivos/deleteDispositivo', dispositivoKey)
+        .then(() => {
+          this.$notify({
+            type: 'success',
+            title: 'Objeto removido com sucesso',
+            closeOnClick: true
+          })
+        })
+        .catch((err) => {
+          this.$notify({
+            type: 'error',
+            title: err.message,
+            closeOnClick: true
+          })
+        })
+    }
+  }
+}
+</script>
