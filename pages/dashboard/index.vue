1e6573a7cbf7e821e14f98a82dbd5c6a7ae6b65c
Heitor Valdeger <aurelio.heitor@hotmail.com>
2021-03-10 22:24:07 -0300

    Criação das tabs em dashboard
    Isto servirá para filtrar os tipos de dispositivos

--- pages/dashboard/index.vue
+++ pages/dashboard/index.vue
@@ -1,41 +1,98 @@
 <template>
   <v-container>
-    <v-row justify="center">
-      <v-col v-for="(item, index) in devicesTypes" :key="index" class="card-header-devices">
-        <v-card outlined :disabled="!!item.disabled" @click="() => {}">
-          <v-card-title class="justify-center">
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-card-title>
-          <v-card-subtitle class="text-center">
-            {{ item.name }}
-          </v-card-subtitle>
-        </v-card>
-      </v-col>
-    </v-row>
+    <v-tabs
+      v-model="activeTab"
+      background-color="teal darken-1"
+      dark
+      icons-and-text
+      grow
+      show-arrows
+      class="tab-rounded"
+    >
+      <v-tabs-slider />
+
+      <v-tab
+        v-for="(item) in tiposDispositivo"
+        :key="item.tipo"
+        :disabled="item.disabled"
+        @click="changeTab(item)"
+      >
+        {{ item.nome }}
+        <v-icon>{{ item.icone }}</v-icon>
+      </v-tab>
+    </v-tabs>
+
+    <v-tabs-items v-model="activeTab">
+      <v-tab-item
+        v-for="i in qtdTiposDispositivos"
+        :key="i"
+      >
+        <template v-if="tipoSelected === 1">
+          <ar-condicionado-card />
+        </template>
+      </v-tab-item>
+    </v-tabs-items>
   </v-container>
 </template>
 
 <script>
+import { mapState } from 'vuex'
 
 export default {
   name: 'DashboardPage',
   layout: 'dashboard',
   middleware: 'auth',
   data: () => ({
-    devicesTypes: [
-      { name: 'Ar Condicionado', icon: 'mdi-air-conditioner' },
-      { name: 'Luzes', icon: 'mdi-lightbulb', disabled: true },
-      { name: 'Caldeira', icon: 'mdi-beaker', disabled: true }
-    ]
+    tiposDispositivo: [
+      {
+        nome: 'Ar Condicionado',
+        icone: 'mdi-air-conditioner',
+        tipo: 1
+      },
+      {
+        nome: 'Luzes',
+        icone: 'mdi-lightbulb',
+        disabled: true,
+        tipo: 2
+      },
+      {
+        nome: 'Caldeira',
+        icone: 'mdi-beaker',
+        disabled: true,
+        tipo: 3
+      }
+    ],
+    activeTab: 0,
+    tipoSelected: 1,
+    text: 'Hello'
   }),
+  computed: {
+    ...mapState({
+      dispositivos: state => state.dispositivos.all
+    }),
+    qtdTiposDispositivos () {
+      return this.tiposDispositivo.length
+    }
+  },
   beforeCreate () {
     this.$store.dispatch('locais/getLocais')
+  },
+  mounted () {
+    console.log(this.tab)
+  },
+  methods: {
+    changeTab ({ tipo }) {
+      this.$store.dispatch('dispositivos/getDispositivos', tipo)
+        .then(() => {
+          this.tipoSelected = tipo
+        })
+    }
   }
 }
 </script>
 
 <style scoped>
-.card-header-devices {
-  min-width: auto;
+.tab-rounded {
+  border-radius: 5px;
 }
 </style>
