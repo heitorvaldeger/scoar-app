afccd1458cb481796561e888a33ca39736c4dbab
heitorvaldeger <aurelio.heitor@hotmail.com>
2021-03-24 18:54:16 -0300

    Importação do componente DispositivosList que permite a renderização para produção

--- pages/dispositivos/index.vue
+++ pages/dispositivos/index.vue
@@ -5,11 +5,15 @@
 </template>
 
 <script>
+import DispositivosList from '~/components/DispositivosList'
 
 export default {
   name: 'DispositivosPage',
   layout: 'dashboard',
   middleware: 'auth',
+  components: {
+    DispositivosList
+  },
   data () {
     return ({
       isAdmin: false
