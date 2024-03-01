03c0f43576f72bcdf09e8f8e969350339bfea6c7
heitorvaldeger <aurelio.heitor@hotmail.com>
2024-03-01 00:24:23 -0300

    feat: create message service to protocols create

--- src/enums/ServiceMessages.ts
+++ src/enums/ServiceMessages.ts
@@ -2,5 +2,9 @@ export enum ServiceMessages {
   PLACE_INVALID_FIELDS = 'Existem dados inválidos!',
   PLACE_CREATE_PENDING = 'Criando local...',
   PLACE_CREATE_SUCCESS = 'Local criado com sucesso!',
-  PLACE_CREATE_ERROR = 'Erro ao criar local'
+  PLACE_CREATE_ERROR = 'Erro ao criar local',
+  PROTOCOL_INVALID_FIELDS = 'Existem dados inválidos para protocolos!',
+  PROTOCOL_CREATE_PENDING = 'Criando protocolo...',
+  PROTOCOL_CREATE_SUCCESS = 'Protocolo criado com sucesso!',
+  PROTOCOL_CREATE_ERROR = 'Erro ao criar protocolo',
 }
